2a6904d4a7943cb0b896706609085add2f7e6758
Suresh <Suresh.Petluri@isowhiz.in>
2023-07-09 12:39:18 +0530

    WIP on boarding Screens

---
+++ lib/app_blocks.dart
@@ -0,0 +1,15 @@
+import 'package:bloc_chat/app_events.dart';
+import 'package:bloc_chat/app_states.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AppBlocs extends Bloc<AppEvents, AppStates> {
+  AppBlocs() : super(InitState()){
+    on<Increment>((event, emit){
+      emit(AppStates(counter: state.counter+1));
+    });
+    on<Decrement>((event, emit){
+      emit(AppStates(counter: state.counter-1));
+    });
+  }
+
+}
